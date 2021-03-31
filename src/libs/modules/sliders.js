02badbfb19c51717f25996190daa04452a7daa0b
flashgrinder <flashgrinder92@gmail.com>
2021-03-31 15:31:56 +0400

    make page service 310321

---
+++ src/libs/modules/sliders.js
@@ -0,0 +1,57 @@
+import Swiper from 'swiper/bundle'
+
+const sliderCases = new Swiper('.cases__swiper', {
+    effect: 'fade',
+    slidesPerView: 1,
+    loop: true,
+    speed: 1000,
+    fadeEffect: {
+        crossFade: true
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteration: false
+    },
+    navigation: {
+        prevEl: '.cases__arrow--right',
+    },
+    on: {
+        slideChange: function(e) {
+            let curSlide = this.slides[this.activeIndex];
+            let preSlide = this.slides[this.previousIndex]
+            curSlide.classList.add('js-animation');
+            preSlide.classList.remove('js-animation');
+        }
+    }
+
+});
+
+const additionalSlider = new Swiper('.additional-serv__swiper', {
+    slidesPerView: 1,
+    // spaceBetween: 0,
+    centeredSlides: true,
+    loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000
+    },
+    navigation: {
+        nextEl: '.additional-serv__arrow--left',
+        prevEl: '.additional-serv__arrow--right'
+    },
+    breakpoints: {
+        767: {
+            slidesPerView: 1,
+            centeredSlides: true,
+        },
+        1024: {
+            slidesPerView: 3,
+            centeredSlides: true,
+        }
+    }
+
+});
+
+
+
+export {additionalSlider, sliderCases}
---
+++ src/libs/scripts.js
@@ -0,0 +1,9 @@
+import {additionalSlider, sliderCases} from "../libs/modules/sliders.js"
+
+window.addEventListener('DOMContentLoaded', (e) => {
+
+    
+    sliderCases.init();
+    additionalSlider.init();
+
+});
